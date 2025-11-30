a3e41aa6d5bf0108642fc9eb1a91ce7ff698bace
d-monkey-CG <138995840+d-monkey-CG@users.noreply.github.com>
2025-11-30 08:51:24 -0600

    Add DCAD Data Files
    This script is used to join together the columns that we actually need for this project from the DCAD data files. The DCAD appraisal values are stored in folders that include many different 'tables' (csv files) so we need to merge certain columns in order to end up with the data (features) we want to use to train the model (in this case I pulled only the columns that will match the data from CCAD and TAD).

---
+++ DCAD Joined 2022-25.py
@@ -0,0 +1,73 @@
+import pandas as pd
+
+# DCAD 2022 Joined script
+file_path_appr = '/Users/joannasanchez/Downloads/DCAD2022_CURRENT/ACCOUNT_APPRL_YEAR.CSV'
+file_path_info = '/Users/joannasanchez/Downloads/DCAD2022_CURRENT/ACCOUNT_INFO.CSV'
+file_path_detail = '/Users/joannasanchez/Downloads/DCAD2022_CURRENT/RES_DETAIL.CSV'
+
+cols_needed_appr = ['ACCOUNT_NUM', 'LAND_VAL', 'TOT_VAL']
+cols_needed_info = ['ACCOUNT_NUM', 'APPRAISAL_YR', 'STREET_NUM', 'STREET_HALF_NUM', 'FULL_STREET_NAME', 'PROPERTY_ZIPCODE']
+cols_needed_detail = ['ACCOUNT_NUM', 'YR_BUILT', 'TOT_MAIN_SF', 'POOL_IND']
+
+df_appr = pd.read_csv(file_path_appr, usecols=cols_needed_appr, low_memory=False)
+df_info = pd.read_csv(file_path_info, usecols=cols_needed_info, low_memory=False)
+df_detail = pd.read_csv(file_path_detail, usecols=cols_needed_detail, low_memory=False)
+
+df_merged = df_appr.merge(df_info, on='ACCOUNT_NUM', how='left')
+df_merged = df_merged.merge(df_detail, on='ACCOUNT_NUM', how='left')
+
+df_merged.to_csv('/Users/joannasanchez/Downloads/DCAD_Joined_2022.csv', index=False)
+
+# DCAD 2023 Joined script
+file_path_appr = '/Users/joannasanchez/Downloads/DCAD2023_CURRENT/ACCOUNT_APPRL_YEAR.CSV'
+file_path_info = '/Users/joannasanchez/Downloads/DCAD2023_CURRENT/ACCOUNT_INFO.CSV'
+file_path_detail = '/Users/joannasanchez/Downloads/DCAD2023_CURRENT/RES_DETAIL.CSV'
+
+cols_needed_appr = ['ACCOUNT_NUM', 'LAND_VAL', 'TOT_VAL']
+cols_needed_info = ['ACCOUNT_NUM', 'APPRAISAL_YR', 'STREET_NUM', 'STREET_HALF_NUM', 'FULL_STREET_NAME', 'PROPERTY_ZIPCODE']
+cols_needed_detail = ['ACCOUNT_NUM', 'YR_BUILT', 'TOT_MAIN_SF', 'POOL_IND']
+
+df_appr = pd.read_csv(file_path_appr, usecols=cols_needed_appr, low_memory=False)
+df_info = pd.read_csv(file_path_info, usecols=cols_needed_info, low_memory=False)
+df_detail = pd.read_csv(file_path_detail, usecols=cols_needed_detail, low_memory=False)
+
+df_merged = df_appr.merge(df_info, on='ACCOUNT_NUM', how='left')
+df_merged = df_merged.merge(df_detail, on='ACCOUNT_NUM', how='left')
+
+df_merged.to_csv('/Users/joannasanchez/Downloads/DCAD_Joined_2023.csv', index=False)
+
+# DCAD 2024 Joined script
+file_path_appr = '/Users/joannasanchez/Downloads/DCAD2024_CURRENT/ACCOUNT_APPRL_YEAR.CSV'
+file_path_info = '/Users/joannasanchez/Downloads/DCAD2024_CURRENT/ACCOUNT_INFO.CSV'
+file_path_detail = '/Users/joannasanchez/Downloads/DCAD2024_CURRENT/RES_DETAIL.CSV'
+
+cols_needed_appr = ['ACCOUNT_NUM', 'LAND_VAL', 'TOT_VAL']
+cols_needed_info = ['ACCOUNT_NUM', 'APPRAISAL_YR', 'STREET_NUM', 'STREET_HALF_NUM', 'FULL_STREET_NAME', 'PROPERTY_ZIPCODE']
+cols_needed_detail = ['ACCOUNT_NUM', 'YR_BUILT', 'TOT_MAIN_SF', 'POOL_IND']
+
+df_appr = pd.read_csv(file_path_appr, usecols=cols_needed_appr, low_memory=False)
+df_info = pd.read_csv(file_path_info, usecols=cols_needed_info, low_memory=False)
+df_detail = pd.read_csv(file_path_detail, usecols=cols_needed_detail, low_memory=False)
+
+df_merged = df_appr.merge(df_info, on='ACCOUNT_NUM', how='left')
+df_merged = df_merged.merge(df_detail, on='ACCOUNT_NUM', how='left')
+
+df_merged.to_csv('/Users/joannasanchez/Downloads/DCAD_Joined_2024.csv', index=False)
+
+# DCAD 2025 Joined script
+file_path_appr = '/Users/joannasanchez/Downloads/DCAD2025_CURRENT/ACCOUNT_APPRL_YEAR.CSV'
+file_path_info = '/Users/joannasanchez/Downloads/DCAD2025_CURRENT/ACCOUNT_INFO.CSV'
+file_path_detail = '/Users/joannasanchez/Downloads/DCAD2025_CURRENT/RES_DETAIL.CSV'
+
+cols_needed_appr = ['ACCOUNT_NUM', 'LAND_VAL', 'TOT_VAL']
+cols_needed_info = ['ACCOUNT_NUM', 'APPRAISAL_YR', 'STREET_NUM', 'STREET_HALF_NUM', 'FULL_STREET_NAME', 'PROPERTY_ZIPCODE']
+cols_needed_detail = ['ACCOUNT_NUM', 'YR_BUILT', 'TOT_MAIN_SF', 'POOL_IND']
+
+df_appr = pd.read_csv(file_path_appr, usecols=cols_needed_appr, low_memory=False)
+df_info = pd.read_csv(file_path_info, usecols=cols_needed_info, low_memory=False)
+df_detail = pd.read_csv(file_path_detail, usecols=cols_needed_detail, low_memory=False)
+
+df_merged = df_appr.merge(df_info, on='ACCOUNT_NUM', how='left')
+df_merged = df_merged.merge(df_detail, on='ACCOUNT_NUM', how='left')
+
+df_merged.to_csv('/Users/joannasanchez/Downloads/DCAD_Joined_2025.csv', index=False)
